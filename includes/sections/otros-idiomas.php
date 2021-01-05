94a69ec91f8451c7b1f9ed92772871bef29daa0a
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2021-01-05 15:23:53 -0600

    section otros-idiomas

---
+++ includes/sections/otros-idiomas.php
@@ -0,0 +1,103 @@
+<link href="css/sections/otros-idiomas.css" rel="stylesheet">
+<section id="otros-idiomas-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <span class="otros-idiomas-span">Otros Idiomas</span>
+                <br><br>
+            </div>
+            <div class="col-md-6">
+                <h3 class="otros-idiomas1-titulo">
+                    Con <span>TECH-LISH</span> aprende o mejora tus habilidades con la
+                    asesoría de profesionales especializados en diversos idiomas.
+                </h3>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section id="otros-idiomas-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Alemán</h5>
+                    <img src="./img/alemania.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La lengua mas hablada en la Unión Europea y el segundo idioma mas usado
+                en sitios web con creciente demanda en el mundo empresarial y de negocios.
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Francés</h5>
+                    <img src="./img/francia.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La lengua diplomática por excelencia, oficial de organismos internacionales como la UE.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Italiano</h5>
+                    <img src="./img/italia.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La lengua de antiguos escritores y pensadores medievales, significativa en el mundo de negocios,
+                gastronómico y artístico.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Chino</h5>
+                    <img src="./img/china.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La lengua con mas número de hablantes nativos y creciente influencia en el ámbito económico y de negocios.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Español</h5>
+                    <img src="./img/espana.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La tercera lengua mas hablada del mundo en los 5 continentes, fundamental
+                en el contexto internacional que te abrirá puertas profesionales y ampliará tu cultura.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Portugués</h5>
+                    <img src="./img/portugal.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La lengua sonora y musical que te abrirá puertas y oportunidades de negocios en varios países.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="otros-idiomas-img-item">
+                    <h5>Japonés</h5>
+                    <img src="./img/japon.png" alt="alemania.png" width="40">
+                </div>
+            </div>
+            <div class="col-md-9 otros-idiomas-text-item">
+                La lengua a la vanguardia en la innovación tecnológica y la segunda mas hablada en Asia.
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -13,12 +13,15 @@
 <?php include('includes/sections/asesoria-ingles.php'); ?>
 <?php $colorOla = "#1e1f3f"; ?>
 <?php include('includes/layout/ola_down.php'); ?>
-
+<?php $colorOla = "#f2f2f2"; ?>
+<?php include('includes/layout/ola_up.php'); ?>
+<?php include('includes/sections/otros-idiomas.php'); ?>
+<?php $colorOla = "#1e1f3f"; ?>
+<?php include('includes/layout/ola_down.php'); ?>
 
 
 <style>
 
 </style>
-
 
 <?php include('includes/layout/foot.php'); ?>
