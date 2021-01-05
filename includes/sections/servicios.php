e65a20168c01103c2e1d9ccb6330e4d243775d34
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2021-01-05 17:20:31 -0600

    section servicios

---
+++ includes/sections/servicios.php
@@ -0,0 +1,60 @@
+<link href="css/sections/servicios.css" rel="stylesheet">
+<section id="servicios">
+    <h3 class="titulo-section">Our Happy Customers</h3>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header servicios-header">
+                        <div class="servicios-img-head-1"></div>
+                    </div>
+                    <div class="card-body servicios-content">
+                        <img class="comillas" src="./img/comillas.svg" width="30" alt="comillas.svg">
+                        <p>
+                            Tech-lish was a great experience for me. 
+                            I had amazing tutors in a professional atmosphere. 
+                            Overall I would recommend this school to anyone 100%
+                        </p>
+                        <h4>Roy Trenneman</h4>
+                        <h5>IT Technical, Reynholm Industries</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header servicios-header">
+                        <div class="servicios-img-head-2"></div>
+                    </div>
+                    <div class="card-body servicios-content">
+                        <img class="comillas" src="./img/comillas.svg" width="30" alt="comillas.svg">
+                        <p>
+                            I prepared the TOEFL exam with Tech-lish. 
+                            High quality of teaching and friendliness of the staff exceed far 
+                            beyond my expectations. Thank you Tech-lish for accompanying me 
+                            through my journey
+                        </p>
+                        <h4>Steward, Alex</h4>
+                        <h5>IT Technical, Reynholm Industries</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header servicios-header">
+                        <div class="servicios-img-head-3"></div>
+                    </div>
+                    <div class="card-body servicios-content">
+                        <img class="comillas" src="./img/comillas.svg" width="30" alt="comillas.svg">
+                        <p>
+                            I’ve been studying at Tech-lish for 6 months. 
+                            I have improved and specialized my English significantly. 
+                            I recommend this school to everyone!
+                        </p>
+                        <h4>Jen Barber</h4>
+                        <h5>Relationship Manager</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,13 +1,26 @@
+<!-- En esta variable se guarda el titulo de la página actual -->
 <?php $pageTitle = "Techlish|Inicio"; ?>
+<!-- La cual se recupera en el archivo head.php en el title -->
 <?php include('includes/layout/head.php'); ?>
+<!-- Aqui se trae la sección head-home que es la del título grandote y la imagen de fondo -->
+<!-- Cada archivo php q esta en sections manda llamar a su css -->
 <?php include('includes/sections/header-home.php'); ?>
+
+
 <?php include('includes/sections/mis-trabajos.php'); ?>
 <?php include('includes/sections/por-que-trabajar.php'); ?>
+<!-- Hay 2 tipos de ola una hacia arriba y otra hacia abajo -->
+<!-- En una variable definimos el color (Gris) y a la hora de traer la hola hacia arriba le pasamos el color -->
 <?php $colorOla = "#f2f2f2"; ?>
 <?php include('includes/layout/ola_up.php'); ?>
+<!-- Y así coincide con el color de fondo de la sigiente sección -->
 <?php include('includes/sections/programa-por-area.php'); ?>
+<!-- Y le ponemos al final una ola hacia abajo pero de diferente color (Azul) ya q así es el fondo del final de la sección-->
 <?php $colorOla = "#1e1f3f"; ?>
 <?php include('includes/layout/ola_down.php'); ?>
+
+
+
 <?php $colorOla = "#f2f2f2"; ?>
 <?php include('includes/layout/ola_up.php'); ?>
 <?php include('includes/sections/asesoria-ingles.php'); ?>
@@ -25,10 +38,8 @@
 <?php include('includes/sections/prueba-ingles.php'); ?>
 <?php $colorOla = "#1e1f3f"; ?>
 <?php include('includes/layout/ola_down.php'); ?>
-
-
-<style>
-
-</style>
+<?php $colorOla = "#f2f2f2"; ?>
+<?php include('includes/layout/ola_up.php'); ?>
+<?php include('includes/sections/servicios.php'); ?>
 
 <?php include('includes/layout/foot.php'); ?>
