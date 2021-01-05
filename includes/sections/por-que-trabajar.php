3cc924cb4eff2cd38a37b099527dde6abd7c40fb
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2021-01-04 20:42:08 -0600

    section por-que-trabajar hotfix

--- includes/sections/por-que-trabajar.php
+++ includes/sections/por-que-trabajar.php
@@ -1,36 +1,54 @@
 <link href="css/sections/por-que-trabajar.css" rel="stylesheet">
 <section id="por-que-trabajar">
-  <div class="contenedor">
-    <h3 class="titulo-section">¿Por que Somos la mejor Opción?</h3>
-    <div class="contenedor-trabajar">
-      <div class="item">
-        <ul class="flex flex-col justify-center items-center">
-          <li class=""><img src="./img/charla.png" alt=""></li>
-          <li><p>No te quedes Incomunicado</p> </li>
-          <li><p>No importa en qué parte del mundo te encuentres, el inglés te permitirá comunicarte con la mitad de la población del planeta de forma efectiva: visita sitios web, lee la prensa, conoce otras culturas, escucha noticias, viaja, accede a programas de estudio internacionales y cursos especializados, disfruta de la música y el cine, usa nuevas tecnologías, haz negocios y mejora tu perspectiva profesional.</p> </li>
-        </ul>
-      </div>
-      <div class="item">
-        <ul class="flex flex-col justify-center items-center">
-          <li><img src="./img/metas.png" alt=""></li>
-          <li>Alcanza tus Metas</li>
-          <li>Desempéñate con seguridad en el mundo empresarial. 
-            Cierra negocios, haz presentaciones, llamadas y entrevistas exitosas. 
-            Eleva tu autoestima y reduce tu nivel de estrés.Permítete viajar y conocer otras culturas.</li>
-        </ul>
-      </div>
-      <div class="item">
-        <ul class="flex flex-col justify-center items-center">
-          <li><img src="./img/objetivo.png" alt=""></li>
-          <li><p>Mejora  Profesionalmente</p> </li>
-          <li><p>90% de las vacantes de trabajo en puestos medios y directivos exigen el inglés como requisito indispensable.
-            88% de los cursos de especialización requieren fluidez en inglés.
-            El inglés aumenta 44% tu posibilidad de ser contratado.
-            Aumenta 20% la rapidez de promoción dentro de tu empresa.
-            Aumenta 18% la posibilidad de obtener un aumento salarial.</p> </li>
-        </ul>
-      </div>
-      
+    <div class="contenedor">
+        <h3 class="titulo-section">¿Por que Somos la mejor Opción?</h3>
     </div>
-  </div>
+    <div class="contaner">
+        <div class="row">
+            <div class="col-md-4 por-que-trabajar-item">
+                <center>
+                    <img src="./img/charla.png" alt="./img/charla.png">
+                </center>
+                <br>
+                <h3 class="text-center">No te quedes Incomunicado</h3>
+                <p>
+                    No importa en qué parte del mundo te encuentres, 
+                    el inglés te permitirá comunicarte con la mitad de la 
+                    población del planeta de forma efectiva: visita sitios web, 
+                    lee la prensa, conoce otras culturas, escucha noticias, 
+                    viaja, accede a programas de estudio internacionales y 
+                    cursos especializados, disfruta de la música y el cine, 
+                    usa nuevas tecnologías, haz negocios y mejora tu perspectiva 
+                    profesional.
+                </p>
+            </div>
+            <div class="col-md-4 por-que-trabajar-item">
+                <center>
+                    <img src="./img/metas.png" alt="./img/metas.png">
+                </center>
+                <br>
+                <h3 class="text-center">Alcanza tus Metas</h3>
+                <p>
+                Desempéñate con seguridad en el mundo empresarial. 
+                Cierra negocios, haz presentaciones, llamadas y entrevistas exitosas. 
+                Eleva tu autoestima y reduce tu nivel de estrés.Permítete viajar y 
+                conocer otras culturas.
+                </p>
+            </div>
+            <div class="col-md-4 por-que-trabajar-item">
+                <center>
+                    <img src="./img/objetivo.png" alt="./img/objetivo.png">
+                </center>
+                <br>
+                <h3 class="text-center">Mejora  Profesionalmente</h3>
+                <p>
+                    90% de las vacantes de trabajo en puestos medios y directivos exigen el inglés como requisito indispensable.
+                    88% de los cursos de especialización requieren fluidez en inglés.
+                    El inglés aumenta 44% tu posibilidad de ser contratado.
+                    Aumenta 20% la rapidez de promoción dentro de tu empresa.
+                    Aumenta 18% la posibilidad de obtener un aumento salarial.
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
